4acbe303fca2b970b08afd07aa0703e8a2ee543d
EzekielJHadley <EzekielJHadley@gmail.com>
2014-11-10 01:20:47 -0800

    added some functionality

--- Sieve of Eratosthenes.py
+++ Sieve of Eratosthenes.py
@@ -13,55 +13,64 @@ and uses the Sieve of Eratosthenes to find the primes
 from sys import argv
 from os.path import exists
 
-#First i check to see if i offer alternate input output files
-if len(argv) == 1:
-	inputName = "input.csv"
-	outputName = "output.csv"
-else:
-	script, inputName, outputName = argv
+def getData(files):
+	#First i check to see if i offer alternate input output files
+	if len(files) == 1:
+		inputName = "input.csv"
+		outputName = "output.csv"
+	else:
+		script, inputName, outputName = files
 	
-#output the files i am reading and writting to
-print "The io files are: ", inputName, "and", outputName
+	#output the files i am reading and writting to
+	print "The io files are: ", inputName, "and", outputName
 
-#open the read and write files
-inputFile = open(inputName)
-outputFile = open(outputName, 'w') #'w' so i can write to it
+	#open the read and write files
+	inputFile = open(inputName)
+	outputFile = open(outputName, 'w') #'w' so i can write to it
 
-#get the numbers to be strained
-data = inputFile.read()
-
-#now separate the numbers out into a list
-data = data.split(',') #the values should be comma separated
-#convert the strings to integers
-values = [int(num) for num in data]
-
-#initialise the list of primes
-prime = []
-
-while True:
-	#the first number is obviously prime
-	prime.append(values.pop(0))
+	#get the numbers to be strained
+	data = inputFile.read()
 	
-	#check to make sure i still have values
-	if(len(values) == 0):
-		break
+	#now close the input file
+	inputFile.close()
+
+	#now separate the numbers out into a list
+	data = data.split(',') #the values should be comma separated
+	#convert the strings to integers
+	values = [int(num) for num in data]
 	
-	#convert it to a set to compare to the set of multiples of out current prime
-	valueSet = set(values)
-	mult = set([prime[-1]*n for n in range(1, int(values[-1]/prime[-1])+1)]) #maxValue/prime + 1 ensures all possible multiples are found
-	#remove the multiples
-	valueSet = valueSet.difference(mult)
-	values = list(valueSet)
-	values.sort() #in-case set rearranges it (which it will)
-	
+	return values, outputFile
+
+
+def primeSieve(values):
+	#initialise the list of primes
+	prime = []
+	while True:
+		#the first number is obviously prime
+		prime.append(values.pop(0))
 		
+		#check to make sure i still have values
+		if(len(values) == 0):
+			break
+		
+		#convert it to a set to compare to the set of multiples of out current prime
+		valueSet = set(values)
+		mult = set([prime[-1]*n for n in range(1, int(values[-1]/prime[-1])+1)]) #maxValue/prime + 1 ensures all possible multiples are found
+		#remove the multiples
+		valueSet = valueSet.difference(mult)
+		values = list(valueSet)
+		values.sort() #in-case set rearranges it (which it will)
+	
+	return prime
+	
+values, outputFile = getData(argv)
+prime = primeSieve(values)
+	
 #now convert the list of primes to a string
-
 primeChar = [str(num) for num in prime]
 line = ",".join(primeChar)
 #output to the file
 outputFile.write(line)
 
 #now close the files
-inputFile.close()
 outputFile.close()
